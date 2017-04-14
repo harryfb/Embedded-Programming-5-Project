f0e3a241729da29a328be7db8ca087c9cbf435a4
Ewan Burns <ewanjburns@btinternet.com>
2017-04-14 17:34:07 +0100

    Update main.cpp

--- FinalQtCameraCode/main.cpp
+++ FinalQtCameraCode/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Window w;
     w.showMaximized();
-
+    //Call the Timer Event function every 25ms
     w.startTimer(25);
 
     return a.exec();
