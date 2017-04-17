fe5a937a26d57ce74d12e8b0924c9f31b40490ab
harryfb <harry.bullough@hotmail.com>
2017-04-17 12:40:18 +0100

    Changed Header

--- FinalQtCameraCode/adcreader.cpp
+++ FinalQtCameraCode/adcreader.cpp
@@ -1,3 +1,18 @@
+/*
+ * AD7705 based Photodiode Camera
+ *
+ * Copyright (c) 2007  MontaVista Software, Inc.
+ * Copyright (c) 2007  Anton Vorontsov <avorontsov@ru.mvista.com>
+ * Copyright (c) 2013-2015  Bernd Porr <mail@berndporr.me.uk>
+ * Copyright (c) 2017 Harry Bullough <2041675b@student.gla.ac.uk
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * For compliation instructions, see README.md
+ */
+
 #include "adcreader.h"
 
 #include <stdint.h>
